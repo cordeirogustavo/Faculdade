af197ec8b5c557556e74afe81209ebedc85b9e44
guhlipule <lipule@gmail.com>
2014-04-01 23:31:17 -0300

    Modificações e adaptações para a implementação da parte 2 da atividade!

--- Pais.php
+++ Pais.php
@@ -1,56 +1,86 @@
 <?php
-class Paises{
-
-
-	private $codigoIso;
+class Pais{
+	private $codigo;
 	private $nome;
 	private $populacao;
 	private $dimensao;
     private $paisVizinho = Array();
          
 
-    public function __construct($codigoIso , $nome, $dimensao){
-		$this->codigoIso = $codigoIso;
+    public function __construct($codigo , $nome, $dimensao){
+		$this->codigo = $codigo;
 		$this->nome = $nome;
 		$this->dimensao = $dimensao;
 	}
-	
-    public function verificaPaisInserido($codigoIso){
-        if ($this->codigoIso == $codigoIso){
-            echo 'Pais já foi inserido, inserir outro!';
-            exit;
-        }     
-    }
-	
-	public function getCodigoIso(){
-		return $this->codigoIso;
+         
+    
+    public function verificaCodigo($codigo){
+            if ($this->codigo == $codigo){
+				return TRUE;
+             }else{ 
+				return FALSE;
+			}
+         }
+          
+	public function setCodigo($codigo){
+		$this->codigo = $codigo;
 	}
-	
-	public function setCodigoIso($codigoIso){
-		$this->codigoIso = $codigoIso;
+	public function getCodigo(){
+		return $this->codigo;
 	}
-	
-	public function getNome(){
-		return $this->nome;
-	}
-	
 	public function setNome($nome){
 		$this->nome = $nome;
 	}
-	
+	public function getNome(){
+		return $this->nome;
+	}
+
+	public function setPopulacao($populacao){
+		$this->populacao = $populacao;
+	}
 	public function getPopulacao(){
 		return $this->populacao;
 	}
-	
-	public function setPopulacao($populacao){
-		$this->populacao = $populacao;
+
+	public function setDimensao($dimensao){
+		$this->dimensao = $dimensao;
 	}
 	
 	public function getDimensao(){
 		return $this->dimensao;
 	} 
 	
-	public function setDimensao($dimensao){
-		$this->dimensao = $dimensao;
+    public function getPaisVizinho(){
+        return $paisVizinho;       
+    }
+	
+	public function setPaisVizinho($paisVizinho){
+		$this->paisVizinho = $paisVizinho;
 	}
+	
+	public function densidadePopulacao(){
+		return $this->populacao / $this->dimensao;
+	}
+	
+	public function buscaFronteira($paisVizinho){
+		foreach($paisVizinho->paisVizinho as $key => $value){
+		if($this->paisVizinho[$key]->verificaCodigo == $paisVizinho->verificaCodigo){
+					echo "O ".$value->nome." faz frenteira com o ". $paisVizinho->nome;
+			}
+		}
+	}
+		
+	public function listarVizinhos($paisVizinho){
+		foreach($this->paisVizinho as $chave => $value1){
+			foreach($paisVizinho->paisVizinho as $key => $value2){
+				if($this->paisVizinho[$chave]->verificaCodigo == $paisVizinho[$key]->verificaCodigo){
+						$lista .= $this->paisVizinho[$chave]->nome.", ";
+				}
+			}
+		}
+		return $lista;
+	}
+}
+
+
 	
